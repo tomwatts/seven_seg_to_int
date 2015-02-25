d33bdf8ec7da6335c04e55c275c5408db0b6f014
Tom Watts <github@tomwatts.ca>
2015-02-25 17:12:25 -0500

    Adding working conversion for one digit.

--- src/SevenSegToInt.java
+++ src/SevenSegToInt.java
@@ -1,16 +1,56 @@
-import java.io.*;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.lang.System;
 
 public class SevenSegToInt {
-	//public static HashTable<String, int> SevenSegInts;
+
+	private static Hashtable<String, Integer> sevenSegInts;
+
+	public static Hashtable<String, Integer> getSevenSegInts() {
+
+		if(sevenSegInts == null) {
+			sevenSegInts = new Hashtable<String, Integer>();
+			sevenSegInts.put(" _ | ||_|", 0);
+			sevenSegInts.put("     |  |", 1);
+			sevenSegInts.put(" _  _||_ ", 2);
+			sevenSegInts.put(" _  _| _|", 3);
+			sevenSegInts.put("   |_|  |", 4);
+			sevenSegInts.put(" _ |_  _|", 5);
+			sevenSegInts.put(" _ |_ |_|", 6);
+			sevenSegInts.put(" _   |  |", 7);
+			sevenSegInts.put(" _ |_||_|", 8);
+			sevenSegInts.put(" _ |_| _|", 9);
+		}
+
+		return sevenSegInts;
+	}
+
 
 	public static int SevenSegStringToInt(String n) {
 
-		return -1;
+		String[] lines = n.split("\n");
+
+		if(lines.length != 3) {
+			// TODO: Throw an error instead
+			return -1;
+		}
+
+		String digit = "";
+		//ArrayList<String> digits = new ArrayList<String>();
+
+		for(int i = 0; i < lines[0].length(); i += 3) {
+
+			digit = lines[0].substring(i, i+3);
+			digit += lines[1].substring(i, i+3);
+			digit += lines[2].substring(i, i+3);
+		}
+
+		return sevenSegInts.get(digit);
 	}
 
 	public static void main(String[] args) {
-
-		System.out.println("Yello!");
+		System.out.println("Done!");
 	}
 }
 
--- src/SevenSegToIntTest.java
+++ src/SevenSegToIntTest.java
@@ -4,9 +4,32 @@ import static org.junit.Assert.*;
 public class SevenSegToIntTest {
 
 	@Test
-	public void alwaysPasses() {
+	public void sevenSegInts_Size_IsTen() {
+		assertEquals(10, SevenSegToInt.getSevenSegInts().size(), 0);
+	}
 
-		assertTrue(SevenSegToInt.SevenSegStringToInt("") == -1);
+	@Test
+	public void sevenSegInts_KeyProvided_ExpectedIntReturned() {
+		assertEquals(0, SevenSegToInt.getSevenSegInts().get(" _ | ||_|"), 0);
+		assertEquals(1, SevenSegToInt.getSevenSegInts().get("     |  |"), 0);
+		assertEquals(2, SevenSegToInt.getSevenSegInts().get(" _  _||_ "), 0);
+		assertEquals(3, SevenSegToInt.getSevenSegInts().get(" _  _| _|"), 0);
+		assertEquals(4, SevenSegToInt.getSevenSegInts().get("   |_|  |"), 0);
+		assertEquals(5, SevenSegToInt.getSevenSegInts().get(" _ |_  _|"), 0);
+		assertEquals(6, SevenSegToInt.getSevenSegInts().get(" _ |_ |_|"), 0);
+		assertEquals(7, SevenSegToInt.getSevenSegInts().get(" _   |  |"), 0);
+		assertEquals(8, SevenSegToInt.getSevenSegInts().get(" _ |_||_|"), 0);
+		assertEquals(9, SevenSegToInt.getSevenSegInts().get(" _ |_| _|"), 0);
+	}
+
+	@Test
+	public void SevenSegStringToInt_StringOne_IntOneReturned() {
+		assertEquals(1, SevenSegToInt.SevenSegStringToInt("   \n  |\n  |"), 0);
+	}
+
+	@Test
+	public void SevenSegStringToInt_StringTwelve_IntTwelveReturned() {
+		assertEquals(12, SevenSegToInt.SevenSegStringToInt("    _ \n   | _|\n  ||_ "), 0);
 	}
 }
 
